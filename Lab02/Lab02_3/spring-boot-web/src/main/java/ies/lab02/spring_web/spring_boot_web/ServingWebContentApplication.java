7f4598f0c055efe4e5bfa286a9e2bcb57af23fed
Pedro Pinto <pmap@ua.pt>
2024-10-08 18:21:09 +0100

    lab02_1,2 and 3 in half

---
+++ Lab02/Lab02_3/spring-boot-web/src/main/java/ies/lab02/spring_web/spring_boot_web/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package ies.lab02.spring_web.spring_boot_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
---
+++ Lab02/Lab02_3/spring-boot-web/src/test/java/ies/lab02/spring_web/spring_boot_web/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.lab02.spring_web.spring_boot_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
